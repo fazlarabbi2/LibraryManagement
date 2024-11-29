7cc3580ea86aedc20ded50e91cf209c880ae749a
fazlarabbi2 <rabbifazla4@gmail.com>
2024-11-30 01:54:18 +0600

    Add project files.

---
+++ LibraryManagement/Views/Borrow/NotAvailable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryManagement.Views.Borrow
+{
+    public class NotAvailableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
